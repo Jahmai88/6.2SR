60866e148c8aaf966cc63f6767ead3bae5e849cb
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 22:26:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Type a non-negative integer:-5 ");
+        System.out.print("Invalid number,try again:-1");
+        System.out.print("Invalid number, try again:11");
+        System.out.print("The square root of 11 is 3.166");
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+    }
+}
